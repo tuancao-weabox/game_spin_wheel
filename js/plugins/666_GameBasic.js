f42862b012572f4f18bb56b534c39f87395ea725
ORIDungeon <88625818+ORIDungeon@users.noreply.github.com>
2023-05-22 07:58:01 +0700

    first commit

---
+++ js/plugins/666_GameBasic.js
@@ -0,0 +1,116 @@
+/**
+ *
+ */
+/**
+ * Update the scene processing each new frame.
+ *
+ * @method update
+ * @instance
+ * @memberof Scene_Base
+ */
+var alias_update_scenebase = Scene_Base.prototype.update;
+Scene_Base.prototype.update = function () {
+  alias_update_scenebase.call(this);
+  if ($gameSystem) {
+    if ($gameSystem._DOMdivContent) {
+      $gameSystem._DOMdivContent.style.width =
+        document.getElementById("GameCanvas").style.width;
+      $gameSystem._DOMdivContent.style.height =
+        document.getElementById("GameCanvas").style.height;
+    }
+  }
+};
+
+var alias_start_Scene_Map = Scene_Map.prototype.start;
+Scene_Map.prototype.start = function () {
+  alias_start_Scene_Map.call(this);
+  if ($gameMap._mapId == 2) {
+    ScreenGamePlay.createSceen();
+  } else {
+    if (!$gamePlayer._endRound) {
+      ScreenMenu.createSceen();
+    }
+  }
+};
+
+Scene_Boot.prototype.start = function () {
+  Scene_Base.prototype.start.call(this);
+  SoundManager.preloadImportantSounds();
+  if (DataManager.isBattleTest()) {
+    DataManager.setupBattleTest();
+    SceneManager.goto(Scene_Battle);
+  } else if (DataManager.isEventTest()) {
+    DataManager.setupEventTest();
+    SceneManager.goto(Scene_Map);
+  } else {
+    this.checkPlayerLocation();
+    DataManager.setupNewGame();
+    SceneManager.goto(Scene_Map);
+    Window_TitleCommand.initCommandPosition();
+  }
+  this.updateDocumentTitle();
+};
+
+//=================================================================================================
+function setInputFilter(textbox, inputFilter, errMsg) {
+  [
+    "input",
+    "keydown",
+    "keyup",
+    "mousedown",
+    "mouseup",
+    "select",
+    "contextmenu",
+    "drop",
+    "focusout",
+  ].forEach(function (event) {
+    textbox.addEventListener(event, function (e) {
+      if (inputFilter(this.value)) {
+        // Accepted value
+        if (["keydown", "mousedown", "focusout"].indexOf(e.type) >= 0) {
+          this.classList.remove("input-error");
+          this.setCustomValidity("");
+        }
+        this.oldValue = this.value;
+        this.oldSelectionStart = this.selectionStart;
+        this.oldSelectionEnd = this.selectionEnd;
+      } else if (this.hasOwnProperty("oldValue")) {
+        // Rejected value - restore the previous one
+        this.classList.add("input-error");
+        this.setCustomValidity(errMsg);
+        this.reportValidity();
+        this.value = this.oldValue;
+        this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+      } else {
+        // Rejected value - nothing to restore
+        this.value = "";
+      }
+    });
+  });
+}
+ReloadImage = () => {
+  for (let i = 2; i <= 8; i++) {
+    ImageManager.loadSystem(i);
+  }
+  // for (let i = 25; i < 40; i++) {
+  //   ImageManager.loadSystem("team" + i);
+  //   ImageManager.loadSystem("EL_ROAD_" + i);
+  // }
+};
+ReloadImage();
+function initApi() {
+  $.ajaxSetup({
+    headers: {
+      "Content-Type": "application/json",
+      lang: "zh-cn",
+    },
+    dataType: "json",
+  });
+  domain = "https://api.waebox.online";
+  if ($gameSystem) {
+    $gameSystem.api = {
+      login: domain + "/api/v1/member/login",
+      register: domain + "/api/v1/member/registry",
+    };
+  }
+}
---
+++ js/plugins/Index.js
@@ -0,0 +1,100 @@
+var Imported = Imported || {};
+Imported.Index = true;
+
+//=============================================================================
+/*:
+ *
+ * @plugindesc
+ *<Index>
+ *
+ * @param color
+ * @desc
+ * @type text
+ * @default
+ *
+ * @param hue
+ * @desc
+ * @type text
+ * @default
+ *
+ * @param bannerHeight
+ * @desc
+ * @type number
+ * @default 15
+ *
+ */
+var parameters = PluginManager.parameters("Index");
+
+/**
+ * Menu
+ */
+const apps = {
+  quayngaytrungthuong: "",
+  vuicung_bsport: "",
+  vongquay_bsport: "",
+  BSport: "",
+  thamgia_bsport: "",
+  choigamecung_BSport: "",
+};
+
+/**
+ * Gameplay
+ */
+ScreenGamePlay = {
+  createSceen: () => {
+    createContent();
+    // createBtnSound(12, 7, 85, -90);
+    let spinWheel = {
+      0: 20,
+      1: 50,
+      2: 0,
+      3: 20,
+      4: 200,
+      5: 50,
+      6: 0,
+      7: 150,
+      8: 100,
+      9: 0,
+    };
+    drawButton($gameSystem._DOMdivContent, "btnPlay", "Quay", "btn", 0, 35);
+    $("#btnPlay").tap(async function () {
+      let speed = getRndInteger(1, 100);
+      let interval = setInterval(() => {
+        $gameScreen._pictures[1]._rotationSpeed += 1;
+        if ($gameScreen._pictures[1]._rotationSpeed == speed) {
+          clearInterval(interval);
+          setTimeout(() => {
+            let interval2 = setInterval(() => {
+              $gameScreen._pictures[1]._rotationSpeed--;
+              if ($gameScreen._pictures[1]._rotationSpeed == 0) {
+                clearInterval(interval2);
+                let a = $gameScreen._pictures[1]._angle / 360;
+                let n = (10 - a * 10) % 10;
+                if (-n > parseInt(-n) + 0.5) {
+                  n = Math.floor(n);
+                } else {
+                  n = parseInt(n);
+                }
+                let res = spinWheel[10 + n];
+                let msg = "";
+                if (res == 0 || !res) {
+                  msg = "Chúc bạn may mắn lần sau";
+                } else {
+                  msg = "Bạn nhận được " + res + "K";
+                }
+                showPopup(msg);
+              }
+            }, 100);
+          }, 3000);
+        }
+      }, 100);
+    });
+    $gameSystem._DOMdivContent.style.background = "none";
+    $gameScreen.showPicture(1, "wheel", 1, 400, 300, 100, 100, 255, 0);
+    $gameScreen.showPicture(2, "arrow", 1, 650, 300, 100, 100, 255, 0);
+  },
+};
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
